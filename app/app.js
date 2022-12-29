a851139d900a9259e70f6a347a34272033f3899f
isyum <saiyum303@gmail.com>
2022-12-29 19:28:14 +0530

    added threads

--- app/app.js
+++ app/app.js
@@ -1,48 +1,73 @@
 var comicShrineApp = angular.module("comicShrine", ['ngRoute']);
 
-comicShrineApp.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-	.when('/home', {
-	    templateUrl: '/views/home.html',
-		controller: 'comicShrineController'
-	})
-	.when('/forum', {
-		templateUrl: '/views/forum.html',
-		controller: 'forumController'
-	})
-	.when('/directory', {
-		templateUrl: '/views/directory.html',
-		controller: 'shrineDirectoryController'
-	})
-	.otherwise({
-	    redirectTo: '/home'
-	})
+comicShrineApp.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+		.when('/home', {
+			templateUrl: '/views/home.html',
+			controller: 'comicShrineController'
+		})
+		.when('/forum', {
+			templateUrl: '/views/forum.html',
+			controller: 'forumController'
+		})
+		.when('/directory', {
+			templateUrl: '/views/directory.html',
+			controller: 'shrineDirectoryController'
+		})
+		.otherwise({
+			redirectTo: '/home'
+		})
 }])
 
 //displaying controller
-comicShrineApp.controller('comicShrineController', ['$scope', '$http', "$location",function($scope, $http, $location){
-	$http.get('https://github.com/shioi/comicShrine/blob/main/data/adventure.json').then(function(response){
+comicShrineApp.controller('comicShrineController', ['$scope', '$http', "$location", function ($scope, $http, $location) {
+	$http.get('./data/adventure.json').then(function (response) {
 		$scope.comics = response.data;
-		for(let i=0;i<$scope.comics.length; i++){
-			$scope.comics[i].coverimage = "https://github.com/shioi/comicShrine/tree/main/data/covers/"+$scope.comics[i].coverimage;
+		for (let i = 0; i < $scope.comics.length; i++) {
+			$scope.comics[i].coverimage = "./data/covers/" + $scope.comics[i].coverimage;
 		}
 	})
 
 	//setting up the clicked forum
-	$scope.setForum = function(comic) {
+	$scope.setForum = function (comic) {
+		alert("Called!!")
 		$scope.selectedComic = {
-			'Title':comic.Title
+			'Title': comic.Title
 		}
-		$location.path("/forum")
+		console.log(comic)
+		sessionStorage.setItem('comic', JSON.stringify(comic));
+		$location.path("forum")
 	}
-	$scope.test = function(){
-		alert("called!!!")
-		console.log($scope.selectedComic);
-	}
-	
+
 }])
 
-comicShrineApp.controller('forumController', ['$scope', '$location', function($scope, $location) {}]);
+comicShrineApp.controller('forumController', ['$scope', '$location', function ($scope) {
+	$scope.comicInfo = JSON.parse(sessionStorage.comic);
+	$scope.opened = false;
+	$scope.formInclude = 'form.html'
+	$scope.allthreads = [];
+	$scope.addThread = function () {
+		if ($scope.opened == false) {
+			$scope.opened = true
+		}
+		else {
+			$scope.formInclude = 'none'
+		}
+	}
 
-comicShrineApp.controller('shrineDirectoryController', ['$scope', function($scope, $location) {}]);
+	$scope.registerThread = function (tname, tdesc) {
+		$scope.allthreads.push(
+			{
+				'Title': tname,
+				'Description': tdesc
+			}
+		)
+	}
+}]);
+
+comicShrineApp.controller('shrineDirectoryController', ['$scope', '$http', "$location", function ($scope, $http, $location) {
+	$http.get('./data/Manga-Data.json').then(function (response) {
+		$scope.mangalist = response.data;
+	})
+}]);
 
